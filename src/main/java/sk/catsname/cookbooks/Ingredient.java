a5105179e1ede79a6766fc73e08022247744e0a0
mmmyster <jakmalicher@gmail.com>
2023-12-10 02:41:53 +0100

    generate basic methods for recipe and ingredient

--- src/main/java/sk/catsname/cookbooks/Ingredient.java
+++ src/main/java/sk/catsname/cookbooks/Ingredient.java
@@ -4,4 +4,16 @@ public class Ingredient {
     private String name;
     private float amount;
     private String unit;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- src/main/java/sk/catsname/cookbooks/Recipe.java
+++ src/main/java/sk/catsname/cookbooks/Recipe.java
@@ -10,4 +10,69 @@ public class Recipe {
     private int servings;
     private List<Ingredient> ingredients;
     private String instructions;
+
+    public Recipe() {
+    }
+
+    public Recipe(String name, Image image, float preparationTime, int servings, List<Ingredient> ingredients, String instructions) {
+        this.name = name;
+        this.image = image;
+        this.preparationTime = preparationTime;
+        this.servings = servings;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public float getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(float preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    @Override
+    public String toString() {
+        return "Recept: " + name + "\nČas na prípravu: " + preparationTime + " minút" + "\nPre " + servings + " osôb" + "\nIngrediencie: " + "\n" + ingredients + "\nPostup: " + instructions;
+    }
 }
