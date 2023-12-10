9f4c45dd911d268089f253f4a49fb48d39fda6bb
SamuelPerzel <perzelsamuel0@gmail.com>
2023-12-11 00:26:49 +0100

    fix sql script

--- db/too_many_cooks.sql
+++ db/too_many_cooks.sql
@@ -1,32 +1,65 @@
--- MySQL Workbench Synchronization
--- Generated: 2023-12-10 22:20
--- Model: New Model
--- Version: 1.0
--- Project: Name of the project
--- Author: perze
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-ALTER SCHEMA `too_many_cookbooks`  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci ;
+-- -----------------------------------------------------
+-- Schema too_many_cookbooks
+-- -----------------------------------------------------
 
-ALTER TABLE `too_many_cookbooks`.`shopping_item` 
-DROP FOREIGN KEY `fk_shopping_list_ingredient1`;
+-- -----------------------------------------------------
+-- Schema too_many_cookbooks
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `too_many_cookbooks` DEFAULT CHARACTER SET utf8 ;
+USE `too_many_cookbooks` ;
 
-ALTER TABLE `too_many_cookbooks`.`ingredient` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+-- -----------------------------------------------------
+-- Table `too_many_cookbooks`.`ingredient`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`ingredient` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
-ALTER TABLE `too_many_cookbooks`.`recipe` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
-ALTER TABLE `too_many_cookbooks`.`cookbook` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
+-- -----------------------------------------------------
+-- Table `too_many_cookbooks`.`recipe`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`recipe` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `image` BLOB NULL,
+  `prep_time` FLOAT NULL,
+  `servings` INT NULL,
+  `instructions` TEXT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `too_many_cookbooks`.`cookbook`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`cookbook` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `image` BLOB NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `too_many_cookbooks`.`ingredient_recipe`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`ingredient_recipe` (
-  `ingredient_id` BIGINT(20) NOT NULL,
-  `recipe_id` BIGINT(20) NOT NULL,
-  `amount` INT(11) NOT NULL,
+  `ingredient_id` BIGINT NOT NULL,
+  `recipe_id` BIGINT NOT NULL,
+  `amount` INT NOT NULL,
   `unit` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`ingredient_id`, `recipe_id`),
   INDEX `fk_ingredients_has_recipe_recipe1_idx` (`recipe_id` ASC) VISIBLE,
@@ -41,15 +74,32 @@ CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`ingredient_recipe` (
     REFERENCES `too_many_cookbooks`.`recipe` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ENGINE = InnoDB;
 
-ALTER TABLE `too_many_cookbooks`.`shopping_item` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
+-- -----------------------------------------------------
+-- Table `too_many_cookbooks`.`shopping_item`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`shopping_item` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `ingredient_id` BIGINT NOT NULL,
+  `amount` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_shopping_list_ingredient1_idx` (`ingredient_id` ASC) VISIBLE,
+  CONSTRAINT `fk_shopping_list_ingredient1`
+    FOREIGN KEY (`ingredient_id`)
+    REFERENCES `too_many_cookbooks`.`ingredient` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `too_many_cookbooks`.`recipe_cookbook`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`recipe_cookbook` (
-  `recipe_id` BIGINT(20) NOT NULL,
-  `cookbook_id` BIGINT(20) NOT NULL,
+  `recipe_id` BIGINT NOT NULL,
+  `cookbook_id` BIGINT NOT NULL,
   PRIMARY KEY (`recipe_id`, `cookbook_id`),
   INDEX `fk_recipe_has_cookbooks_cookbooks1_idx` (`cookbook_id` ASC) VISIBLE,
   INDEX `fk_recipe_has_cookbooks_recipe1_idx` (`recipe_id` ASC) VISIBLE,
@@ -63,15 +113,7 @@ CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`recipe_cookbook` (
     REFERENCES `too_many_cookbooks`.`cookbook` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-ALTER TABLE `too_many_cookbooks`.`shopping_item` 
-ADD CONSTRAINT `fk_shopping_list_ingredient1`
-  FOREIGN KEY (`ingredient_id`)
-  REFERENCES `too_many_cookbooks`.`ingredient` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
