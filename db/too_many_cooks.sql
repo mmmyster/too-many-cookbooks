28a9aa343a03c3761db4412f95afac806e26e08a
SamuelPerzel <perzelsamuel0@gmail.com>
2023-12-20 21:35:02 +0100

    edit sql script

--- db/too_many_cooks.sql
+++ db/too_many_cooks.sql
@@ -30,10 +30,10 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`recipe` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
-  `image` MEDIUMBLOB NULL,
-  `prep_time` FLOAT NULL,
-  `servings` INT NULL,
-  `instructions` TEXT NULL,
+  `image` MEDIUMBLOB NULL DEFAULT NULL,
+  `prep_time` FLOAT NULL DEFAULT NULL,
+  `servings` INT NULL DEFAULT NULL,
+  `instructions` TEXT NULL DEFAULT NULL,
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`))
@@ -46,7 +46,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`cookbook` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
-  `image` MEDIUMBLOB NULL,
+  `image` MEDIUMBLOB NULL DEFAULT NULL,
   `created_at` DATETIME NOT NULL,
   `updated_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`))
@@ -59,11 +59,11 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`ingredient_recipe` (
   `ingredient_id` BIGINT NOT NULL,
   `recipe_id` BIGINT NOT NULL,
-  `amount` INT NOT NULL,
+  `amount` FLOAT NOT NULL,
   `unit` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`ingredient_id`, `recipe_id`),
-  INDEX `fk_ingredients_has_recipe_recipe1_idx` (`recipe_id` ASC),
-  INDEX `fk_ingredients_has_recipe_ingredients_idx` (`ingredient_id` ASC),
+  INDEX `fk_ingredients_has_recipe_recipe1_idx` (`recipe_id` ASC) VISIBLE,
+  INDEX `fk_ingredients_has_recipe_ingredients_idx` (`ingredient_id` ASC) VISIBLE,
   CONSTRAINT `fk_ingredients_has_recipe_ingredients`
     FOREIGN KEY (`ingredient_id`)
     REFERENCES `too_many_cookbooks`.`ingredient` (`id`)
@@ -83,9 +83,9 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`shopping_item` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `ingredient_id` BIGINT NOT NULL,
-  `amount` INT NOT NULL,
+  `amount` FLOAT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_shopping_list_ingredient1_idx` (`ingredient_id` ASC),
+  INDEX `fk_shopping_list_ingredient1_idx` (`ingredient_id` ASC) VISIBLE,
   CONSTRAINT `fk_shopping_list_ingredient1`
     FOREIGN KEY (`ingredient_id`)
     REFERENCES `too_many_cookbooks`.`ingredient` (`id`)
@@ -101,8 +101,8 @@ CREATE TABLE IF NOT EXISTS `too_many_cookbooks`.`recipe_cookbook` (
   `recipe_id` BIGINT NOT NULL,
   `cookbook_id` BIGINT NOT NULL,
   PRIMARY KEY (`recipe_id`, `cookbook_id`),
-  INDEX `fk_recipe_has_cookbooks_cookbooks1_idx` (`cookbook_id` ASC),
-  INDEX `fk_recipe_has_cookbooks_recipe1_idx` (`recipe_id` ASC),
+  INDEX `fk_recipe_has_cookbooks_cookbooks1_idx` (`cookbook_id` ASC) VISIBLE,
+  INDEX `fk_recipe_has_cookbooks_recipe1_idx` (`recipe_id` ASC) VISIBLE,
   CONSTRAINT `fk_recipe_has_cookbooks_recipe1`
     FOREIGN KEY (`recipe_id`)
     REFERENCES `too_many_cookbooks`.`recipe` (`id`)
